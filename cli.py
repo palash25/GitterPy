75cbe9df8fdfc1dd15414bacb0fe70f857dc62f3
Palash Nigam <npalash25@gmail.com>
2017-12-28 20:27:46 +0530

    GIF added

--- cli.py
+++ cli.py
@@ -16,7 +16,19 @@ def cli(config):
 ###############Welcome to GitterPy##############\n
 Your command line client to interact with gitter\n    
 ################################################\n
-  This tool can fetch and post info to gitter\n
+  This tool can fetch and post info to gitter\n\n
+##################User Manual###################\n
+Currently this tool provides two commands `get` and `post`\n
+
+Fetching user info: gitterpy get [--info] [argument]\n
+The info argument can be:\n
+1) [u] -> To fetch user's profile\n
+2) [r] -> To fetch the rooms\n
+3) [g] -> To fetch groups\n
+4) [m] -> To fetch messages\n\n
+
+Post a message to a room: gitterpy post [--message] [text]\n
+[text] -> It is the message you want to post in enclosed by quotes
     '''
     pass
 
@@ -25,17 +37,22 @@ Your command line client to interact with gitter\n
 @click.option('--message', type=str)
 @pass_config
 def post(config, message):
-    click.echo(config.ob.post(message))
+    status = config.ob.post(message)
+    if status == 200:
+        click.echo('Your message was successfully posted, Status Code: {}'.format(status))
+    else:
+        click.echo('Your message could not be posted, Status Code: {}'.format(status))
 
 
 @cli.command()
 @click.option('--info', default='u', help='Command to fetch info'
 'based on the argument passed')
+@click.option('--json', type=bool, default=True)
 @pass_config
-def get(config, info):
+def get(config, info, json):
 	gp = GitterPy()
 	if info=='u':
-		click.echo(config.ob.get_user())
+		click.echo(config.ob.get_user(json))
 	elif info=='r':
 		click.echo(config.ob.get_rooms())
 	elif info=='g':
--- gitter.py
+++ gitter.py
@@ -9,23 +9,27 @@ class GitterPy(object):
     def __init__(self):
         pass
 
-    def _get(self, path, query=None):
+    def _get(self, path, format):
         self.path = path
         _url = URL+self.path
-        _response = requests.get(_url, params=PAYLOAD).json()
-        #_response = json.loads(_r.text)
-        #_response = json.dumps(_user_data, indent =4, sort_keys=True) # str
+        _r = requests.get(_url, params=PAYLOAD)
+        if format == False:
+            _response = _r.json()
+        else:
+            _data = json.loads(_r.text)
+            _response = json.dumps(_data, indent =4, sort_keys=True) # str
         return _response
 
     def _post(self, path, data):
-        _url = URL+path
+        self.path = path
         self.data = data
+        _url = URL+path
         r = requests.post('https://api.gitter.im/v1/rooms/5a3d468fd73408ce4f848340/chatMessages',
 		headers=HEADERS, data=self.data)
         return r.status_code
 
-    def get_user(self):
-        response = self._get('/user')
+    def get_user(self, json):
+        response = self._get('/user', json)
         print(type(response), len(response))
         return response
 
